735549aa32dd6bcdb614672b5c57b7ac3f57ce3f
Зайнагтдинова Галия Нафисовна <GZaynagtdinova@beeline.ru>
2024-01-09 17:31:33 +0300

    refactor

--- src/main/java/ru/beeline/capability/controller/ApplicationController.java
+++ src/main/java/ru/beeline/capability/controller/ApplicationController.java
@@ -1,10 +1,12 @@
 package ru.beeline.capability.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "Application", tags = "Application")
 public class ApplicationController {
 
     @Value("${app.version}")
