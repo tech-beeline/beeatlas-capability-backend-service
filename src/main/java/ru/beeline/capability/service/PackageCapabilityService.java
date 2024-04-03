0d526ff7134c8a487b6910fb90949167c3371e12
Зайнагтдинова Галия Нафисовна <GZaynagtdinova@beeline.ru>
2024-04-03 09:54:39 +0300

    SFDM-1345 : fix API method packLoadTechCapabilities

---
+++ src/main/java/ru/beeline/capability/service/PackageCapabilityService.java
@@ -0,0 +1,42 @@
+package ru.beeline.capability.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import ru.beeline.capability.dto.PostTechCapabilityDTO;
+import ru.beeline.capability.dto.RegisteredCapabilityPackageDTO;
+
+import java.util.List;
+
+import static ru.beeline.capability.utils.Constants.UPDATE_TECH_CAPABILITIES_OPERATION;
+import static ru.beeline.capability.utils.Constants.PACK_LOADER_SERVICE_URL;
+import static ru.beeline.capability.utils.RestHelper.getRestTemplate;
+
+@Slf4j
+@Service
+public class PackageCapabilityService {
+
+    public RegisteredCapabilityPackageDTO registerTechCapabilitiesPackage(List<PostTechCapabilityDTO> techCapabilities) {
+        try {
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            MultiValueMap<String, String> bodyParamMap = new LinkedMultiValueMap<>();
+            bodyParamMap.add("operation", UPDATE_TECH_CAPABILITIES_OPERATION);
+            bodyParamMap.add("count", String.valueOf(techCapabilities.size()));
+
+            HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(bodyParamMap, headers);
+            final RestTemplate restTemplate = getRestTemplate();
+
+            return restTemplate.exchange(PACK_LOADER_SERVICE_URL + "/api/v1/package",
+                    HttpMethod.POST, entity, RegisteredCapabilityPackageDTO.class).getBody();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return null;
+    }
+}
