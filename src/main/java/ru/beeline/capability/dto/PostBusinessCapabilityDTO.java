e4e2edc604a89527eb2f4b3918ce36e61523f460
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2024-06-17 20:47:11 +0300

    SFDM-1509 Правки по ДТО

--- src/main/java/ru/beeline/capability/dto/PostBusinessCapabilityDTO.java
+++ src/main/java/ru/beeline/capability/dto/PostBusinessCapabilityDTO.java
@@ -2,6 +2,8 @@ package ru.beeline.capability.dto;
 
 import lombok.*;
 
+import java.util.Date;
+
 @Setter
 @Getter
 @NoArgsConstructor
@@ -9,12 +11,14 @@ import lombok.*;
 @Builder
 public class PostBusinessCapabilityDTO {
     private String code;
+    private Boolean isDomain;
     private String name;
     private String description;
+    private Date modifiedDate;
+    private Date createdDate;
     private String author;
     private String link;
     private String owner;
     private String status;
     private String parent;
-    private Boolean isDomain;
 }
