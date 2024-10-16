14e8e237991b6861cdf1b94c9c587fc3b8672078
Алиханов Сафарбек Йулдашалиевич <salikhanov@oms.beeline.ru>
2024-10-16 11:07:27 +0000

    SFDM-1908 Разработать метод PATCH изменить карту

---
+++ src/main/java/ru/beeline/capability/domain/Group.java
@@ -0,0 +1,36 @@
+package ru.beeline.capability.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "groups", schema = "capability")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "groups_generator")
+    @SequenceGenerator(name = "groups_generator", sequenceName = "groups_id_seq", allocationSize = 1)
+    private Integer id;
+
+    private String name;
+
+    @Column(name = "map_id")
+    private Integer mapId;
+
+    @Column(name = "parent_id")
+    private String parentId;
+}
