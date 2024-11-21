40e571774acfb30039b512e436d42f3ebfb39627
salikhanov <SAlikhanov@oms.beeline.ru>
2024-11-21 16:32:29 +0600

    SFDM-2135 Исправлено название атрибута  GET/capability/v1/maps

--- src/main/java/ru/beeline/capability/dto/GetCapabilityMapByIdDTO.java
+++ src/main/java/ru/beeline/capability/dto/GetCapabilityMapByIdDTO.java
@@ -14,6 +14,6 @@ public class GetCapabilityMapByIdDTO {
 
     private String name;
     private String description;
-    private EntityType entityType;
+    private EntityType type;
     private List<GroupDTO> groups;
 }
