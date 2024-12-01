d19515b90b2c874e6636ed580a6756d47dc104f4
salikhanov <SAlikhanov@oms.beeline.ru>
2024-12-01 14:47:50 +0600

    SFDM-2094 Разработать метод GET /business-capability/history/compare/{id}/{version}?other_version={}

---
+++ src/main/java/ru/beeline/capability/dto/ParentDTO.java
@@ -0,0 +1,17 @@
+package ru.beeline.capability.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ParentDTO {
+
+    private Long id;
+    private String code;
+    private String name;
+}
