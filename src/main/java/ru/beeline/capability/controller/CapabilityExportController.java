e909bd8c6c7b6d074df1708b550186c2cf405da8
Алиханов Сафарбек Йулдашалиевич <salikhanov@oms.beeline.ru>
2025-02-14 09:04:57 +0000

    SFDM-2352 Разработать метод экспорта BC GET /api/v1/export/business-capability

--- src/main/java/ru/beeline/capability/controller/CapabilityExportController.java
+++ src/main/java/ru/beeline/capability/controller/CapabilityExportController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.beeline.capability.dto.CapabilityExportDTO;
 import ru.beeline.capability.service.CapabilityExportService;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -17,7 +18,7 @@ public class CapabilityExportController {
 
     @GetMapping("/business-capability/{doc_id}")
     @ApiOperation(value = "get export Business Capabilities")
-    public ResponseEntity getExportBusinessCapabilities(@PathVariable(name = "doc_id") Integer docId) {
+    public ResponseEntity<CapabilityExportDTO> getExportBusinessCapabilities(@PathVariable(name = "doc_id") Integer docId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(capabilityExportService.getExportBusinessCapabilities(docId));
     }
 }
