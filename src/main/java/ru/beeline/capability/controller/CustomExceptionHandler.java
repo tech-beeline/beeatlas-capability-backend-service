cf4423ae3c638c56f01e08932235a0a8fc6a19ac
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2025-06-26 09:42:46 +0300

    SFDM-2628 Добавить описания ошибок в ответы для метода PATCH /api/v1/business-capability/order/{id}?statusAlias

--- src/main/java/ru/beeline/capability/controller/CustomExceptionHandler.java
+++ src/main/java/ru/beeline/capability/controller/CustomExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
 import ru.beeline.capability.exception.DocumentServerException;
 import ru.beeline.capability.exception.ForbiddenException;
 import ru.beeline.capability.exception.NotFoundException;
@@ -20,6 +21,12 @@ public class CustomExceptionHandler {
     public ResponseEntity<Object> handleException(IllegalArgumentException e) {
         log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("400 BAD_REQUEST : " + e.getMessage());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Object> handleException(ResponseStatusException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(e.getStatus()).body(e.getMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
