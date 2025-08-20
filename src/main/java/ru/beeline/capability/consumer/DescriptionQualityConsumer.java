5d4fb2472f136a169a76553bb467dc8d0eaeaba3
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2025-08-20 10:55:58 +0300

    Fix delay rabbit

--- src/main/java/ru/beeline/capability/consumer/DescriptionQualityConsumer.java
+++ src/main/java/ru/beeline/capability/consumer/DescriptionQualityConsumer.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.beeline.capability.service.QualityService;
 
@@ -15,16 +16,21 @@ import ru.beeline.capability.service.QualityService;
 @EnableRabbit
 public class DescriptionQualityConsumer {
 
+    @Value("${integration.bpm-server-url}")
+    private final int delayConsumer;
+
     @Autowired
     QualityService qualityService;
     private ObjectMapper objectMapper = new ObjectMapper();
 
+    public DescriptionQualityConsumer(int delayConsumer) {this.delayConsumer = delayConsumer;}
+
     @RabbitListener(queues = "${queue.tc-description-quality.name}")
     public void techQueue(String message) {
         log.info("Received message from tc-description-quality queue: " + message);
-
         JsonNode jsonNode;
         try {
+            Thread.sleep(delayConsumer);
             jsonNode = objectMapper.readTree(message);
             if (!jsonNode.has("changeType") || !jsonNode.has("entityId")) {
                 log.error("Message does not match the required format: " + message);
